f12abccf31996a71dc447c7b7e4818296f201f56
Denis <denis.dylko@gmail.com>
2019-06-13 13:17:08 +0300

    .quickSort added

--- sorter.h
+++ sorter.h
@@ -2,6 +2,7 @@
 #define SORTER_H
 
 #include <functional>
+#include <math.h>
 #include <vector>
 
 template<typename T>
@@ -23,8 +24,42 @@ public:
         return srcArray.size();
     }
 
+    static size_t getMainIndex( size_t len ){
+        return static_cast<size_t>(floor(static_cast<float>(len)* 0.62));
+    }
 
-    void quickSort(){}
+    static size_t partition( std::vector<T>& srcArray, size_t left, size_t main, size_t right,
+                    std::function<bool(T& l, T& r)> predicate ){
+        auto value = srcArray[main];
+        std::iter_swap( srcArray.begin() + main, srcArray.begin() + right );
+
+        auto storeIndex = srcArray.begin() + left;
+
+        for( auto it = srcArray.begin() + left; it != srcArray.begin() + right; it++ ){
+            if( predicate(*it, value) ){
+                std::iter_swap(it, storeIndex);
+                storeIndex += 1;;
+            }
+        }
+
+        std::iter_swap(srcArray.begin() + right, storeIndex);
+        return ( storeIndex - srcArray.begin() );
+    }
+
+
+    static size_t quickSort( std::vector<T>& srcArray, size_t l, size_t r,
+                    std::function<bool(T& l, T& r)> predicate ){
+        if( l < r ){
+            auto index = l + getMainIndex(r - l);
+            auto newIndex = partition(srcArray, l, index, r, predicate);
+
+            quickSort(srcArray, l, newIndex - 1, predicate);
+            quickSort(srcArray, newIndex + 1, r, predicate);
+        }
+
+        return srcArray.size();
+
+    }
     void joinSort(){}
 
 };
